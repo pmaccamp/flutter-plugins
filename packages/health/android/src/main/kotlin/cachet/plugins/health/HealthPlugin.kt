ac5720c9c0e07e702f9b9a1fb7954caf63695884
eliasteeny <elias.teeny@gmail.com>
2022-01-20 23:01:34 +0200

    bug fix

--- packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ packages/health/android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -281,16 +281,16 @@ class HealthPlugin(private var channel: MethodChannel? = null) : MethodCallHandl
                 .setField(Field.FIELD_MEAL_TYPE, keyToMealType(mealType))
 
         val nutrients = mapOf(
-                Field.NUTRIENT_TOTAL_FAT to call.argument<Float>("totalFat"),
-                Field.NUTRIENT_SODIUM to call.argument<Float>("totalSodium"),
-                Field.NUTRIENT_SATURATED_FAT to call.argument<Float>("totalSaturatedFat"),
-                Field.NUTRIENT_PROTEIN to call.argument<Float>("totalProtein"),
-                Field.NUTRIENT_TOTAL_CARBS to call.argument<Float>("totalTotalCarbs"),
-                Field.NUTRIENT_CHOLESTEROL to call.argument<Float>("totalCholesterol"),
-                Field.NUTRIENT_CALORIES to call.argument<Float>("totalCalories"),
-                Field.NUTRIENT_SUGAR to call.argument<Float>("totalSugar"),
-                Field.NUTRIENT_DIETARY_FIBER to call.argument<Float>("totalDietaryFiber"),
-                Field.NUTRIENT_POTASSIUM to call.argument<Float>("totalPotassium")
+                Field.NUTRIENT_TOTAL_FAT to call.argument<Double>("totalFat")!!.toFloat(),
+                Field.NUTRIENT_SODIUM to call.argument<Double>("totalSodium")!!.toFloat(),
+                Field.NUTRIENT_SATURATED_FAT to call.argument<Double>("totalSaturatedFat")!!.toFloat(),
+                Field.NUTRIENT_PROTEIN to call.argument<Double>("totalProtein")!!.toFloat(),
+                Field.NUTRIENT_TOTAL_CARBS to call.argument<Double>("totalTotalCarbs")!!.toFloat(),
+                Field.NUTRIENT_CHOLESTEROL to call.argument<Double>("totalCholesterol")!!.toFloat(),
+                Field.NUTRIENT_CALORIES to call.argument<Double>("totalCalories")!!.toFloat(),
+                Field.NUTRIENT_SUGAR to call.argument<Double>("totalSugar")!!.toFloat(),
+                Field.NUTRIENT_DIETARY_FIBER to call.argument<Double>("totalDietaryFiber")!!.toFloat(),
+                Field.NUTRIENT_POTASSIUM to call.argument<Double>("totalPotassium")!!.toFloat()
         )
 
         builder.setField(Field.FIELD_NUTRIENTS, nutrients)
